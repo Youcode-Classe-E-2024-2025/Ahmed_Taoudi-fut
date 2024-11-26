290a5292e9a532efe743265a0da088d9f5f86a8d
Tawdi <ahmedtawdi00@gmail.com>
2024-11-26 14:20:56 +0100

    cart de joueur v.1

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,13 +11,26 @@ fetch("/assets/data/players.json")
 .then( data =>{
    if(data){ console.log(data);
     list =data.players;
-console.log("list",list);
-    dispalyPlayers(data)
+    // console.log("list",list);
+    dispalyPlayers(list);
    }
 }
 )
 .catch(err => console.error("error de fetch ",err))
 
-function dispalyPlayers(data){
+function dispalyPlayers(list){
 
+    let cart ;
+    list.forEach(plyr => {
+        cart = document.createElement("div");
+        cart.className="cart "
+        cart.innerHTML=`
+            <img class="w-full " src="./assets/img/badge_gold.webp" alt="">
+div
+        <img class="w-full " src="${plyr.photo}" alt="photo of ${plyr.name}">
+        <p class="font-bold ">${plyr.name}</p>
+        `
+        document.getElementById('playerList').append(cart);
+
+    });
 }
