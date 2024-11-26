396585bbfb438e70d358bae15fe58cf0a573db15
Tawdi <ahmedtawdi00@gmail.com>
2024-11-26 15:20:37 +0100

    ajoute de fonction displayPlayer

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,17 +20,57 @@ fetch("/assets/data/players.json")
 
 function dispalyPlayers(list){
 
-    let cart ;
-    list.forEach(plyr => {
-        cart = document.createElement("div");
-        cart.className="cart "
-        cart.innerHTML=`
-            <img class="w-full " src="./assets/img/badge_gold.webp" alt="">
-div
-        <img class="w-full " src="${plyr.photo}" alt="photo of ${plyr.name}">
-        <p class="font-bold ">${plyr.name}</p>
-        `
-        document.getElementById('playerList').append(cart);
 
+    let cart ; 
+    list.forEach(plyr => { 
+        cart = document.createElement("div");
+        cart.className="cart w-full "
+        cart.innerHTML=`
+           <img class="w-full" src="./assets/img/badge_gold.webp" alt="">
+                <div class="w-full h-full flex flex-col items-center justify-center  absolute right-0 top-0 ">
+                  <img class="w-2/3"  src="${plyr.photo}" alt="photo of ${plyr.name}">
+                  <p class="font-semibold text-[3vw]  sm:text-[10px] ">${plyr.name}</p>
+                  <ul class="flex  w-[75%] justify-between">
+                    
+                    <li >
+                        <p class="   p-0 text-[2vw] sm:text-[0.5rem] lh">${"PAC"}
+                            <br>
+                            <span class="text-[2.5vw] sm:text-[0.5rem]">${plyr.pace}</span>
+                        </p>
+                    </li>
+                    <li>
+                        <p class="   p-0 text-[2vw] sm:text-[0.5rem] lh">SHO
+                            <br>
+                            <span class="text-[2.5vw] sm:text-[0.5rem]">${plyr.shooting}</span>
+                        </p>    
+                    </li>
+                    <li>
+                        <p class="   p-0 text-[2vw] sm:text-[0.5rem] lh">DRI
+                            <br>
+                            <span class="text-[2.5vw] sm:text-[0.5rem]">${plyr.dribbling}</span>
+                        </p>    
+                    </li>
+                    <li>
+                        <p class="   p-0 text-[2vw] sm:text-[0.5rem] lh">PAS
+                            <br>
+                            <span class="text-[2.5vw] sm:text-[0.5rem]">${plyr.passing}</span>
+                        </p>    
+                    </li>
+                    <li>
+                        <p class="   p-0 text-[2vw] sm:text-[0.5rem] lh">DEF
+                            <br>
+                            <span class="text-[2.5vw] sm:text-[0.5rem]">${plyr.defending}</span>
+                        </p>    
+                    </li>
+                    <li>
+                        <p class="   p-0 text-[2vw] sm:text-[0.5rem] lh">PHY
+                            <br>
+                            <span class="text-[2.5vw] sm:text-[0.5rem]">${plyr.physical}</span>
+                        </p>    
+                    </li>
+                  </ul>
+                </div> `
+        document.getElementById('playerList').append(cart);
+    
     });
 }
