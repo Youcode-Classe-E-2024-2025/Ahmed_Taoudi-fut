2a40683e80fa9efb783d712881372c889001e14d
Tawdi <ahmedtawdi00@gmail.com>
2024-11-27 10:24:00 +0100

    separer le code ceation des cart avec des module (export/import)

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,5 @@
+import { createCArt } from "./modules/playerCart.js";
+
 console.log("aaaaaa");
 let list =[]
 fetch("/assets/data/players.json")
@@ -21,72 +23,6 @@ fetch("/assets/data/players.json")
 function dispalyPlayers(list){
 
 
-    let cart ; 
-    let test;
-    list.forEach(plyr => { 
-        if(plyr.position!="GK"){test=1; }else{ test=0;}
-        cart = document.createElement("div");
-        cart.className="cart w-full "
-        cart.innerHTML=`
-           <img class="w-full" src="./assets/img/badge_gold.webp" alt="">
-                <div class="w-full h-full flex flex-col items-center justify-center  absolute right-0 top-0 ">
-                 <div class="w-full  ">
-           <img class="mx-auto w-2/3"  src="${plyr.photo}" alt="photo of ${plyr.name}">
-           <p class=" absolute top-[25%] left-[15%]   p-0 text-[2vw] sm:text-[0.5rem] font-semibold ">${plyr.position}
-            <br>
-            <span class="text-[4vw] sm:text-[1rem] font-extrabold">${ plyr.rating}</span>
-        </p>
-       </div>
-                <p class="font-semibold text-[3vw]  sm:text-[0.8rem] ">${plyr.name}</p>
-                  <ul class="flex px-2  w-[75%] justify-between">
-                    
-                    <li >
-                        <p class="   p-0 text-[2vw] sm:text-[0.4rem] font-semibold ">${test?"PAC":"DIV"}
-                            <br>
-                            <span class="text-[2.5vw] sm:text-[0.6rem] font-extrabold"> ${test ? plyr.pace:plyr.diving}</span>
-                        </p>
-                    </li>
-                    <li>
-                        <p class="   p-0 text-[2vw] sm:text-[0.4rem] font-semibold ">${test?"SHO":"HAN"}
-                            <br>
-                            <span class="text-[2.5vw] sm:text-[0.6rem] font-extrabold">${test ? plyr.shooting:plyr.handling}</span>
-                        </p>    
-                    </li>
-                    <li>
-                        <p class="   p-0 text-[2vw] sm:text-[0.4rem] font-semibold ">${test?"DRI":"KIC"}
-                            <br>
-                            <span class="text-[2.5vw] sm:text-[0.6rem] font-extrabold">${test ?plyr.dribbling:plyr.kicking}</span>
-                        </p>    
-                    </li>
-                    <li>
-                        <p class="   p-0 text-[2vw] sm:text-[0.4rem] font-semibold ">${test?"PAS":"REF"}
-                            <br>
-                            <span class="text-[2.5vw] sm:text-[0.6rem] font-extrabold">${test ?plyr.passing:plyr.reflexes}</span>
-                        </p>    
-                    </li>
-                    <li>
-                        <p class="   p-0 text-[2vw] sm:text-[0.4rem] font-semibold ">${test?"DEF":"SPE"}
-                            <br>
-                            <span class="text-[2.5vw] sm:text-[0.6rem] font-extrabold">${test ?plyr.defending:plyr.speed}</span>
-                        </p>    
-                    </li>
-                    <li>
-                        <p class="   p-0 text-[2vw] sm:text-[0.4rem] font-semibold ">${test?"PHY":"POS"}
-                            <br>
-                            <span class="text-[2.5vw] sm:text-[0.6rem] font-extrabold">${test ?plyr.physical:plyr.positioning}</span>
-                        </p>    
-                    </li>
-                  </ul>
-                   <div class=" w-full justify-center items-center gap-6 flex">
-        <div class="w-1/12" >
-            <img class="w-full" src=" ${plyr.logo} " alt="${plyr.club}">
-        </div>
-        <div class="w-1/12" >
-            <img class="w-full" src="${plyr.flag}" alt="${plyr.nationality}">
-        </div>
-       </div>
-                </div> `
-        document.getElementById('playerList').append(cart);
     
-    });
+    list.forEach(plyr =>  createCArt(plyr));
 }
