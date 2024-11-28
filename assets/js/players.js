64e04749e551bc68bd12c9b5601fc4124019f1a5
Tawdi <ahmedtawdi00@gmail.com>
2024-11-28 22:38:35 +0100

    ajoute fonction de supprimer

--- assets/js/players.js
+++ assets/js/players.js
@@ -1,92 +1,78 @@
 import { createCArt } from "./modules/playerCart.js";
-
+import { dispalyPlayers, displayDetails, getData, removeFromList } from "./modules/CRUD.js";
 let list = JSON.parse(localStorage.getItem('FUT-players')) || [];
-function dispalyPlayers(list){
-    
-    
-    
-    list.forEach(plyr =>  {
-        document.getElementById('playerList2').append(createCArt(plyr))
-    });
-}
-dispalyPlayers(list)
+
+dispalyPlayers(list,'playerList2')
 // console.log(d);
 document.addEventListener('DOMContentLoaded',()=>{
     const carts=document.querySelectorAll(".p-cart");
     
-    carts.forEach(cart=>{
-        cart.addEventListener('click',()=>{
-            openModelForm();
-        })
-    })
+    
 
 })
-function openModelForm(){
+function openModelForm(form="add"){
     document.getElementById('overlay').classList.remove('hidden')
+    if(form=="add"){
+        document.getElementById('modal-form-edit').classList.add('hidden')
+       document.getElementById('modal-form-add').classList.remove('hidden')
+    }else if(form=="edit"){
+        document.getElementById('modal-form-add').classList.add('hidden')
+        document.getElementById('modal-form-edit').classList.remove('hidden')
+    }
 }
 document.getElementById('btn-cancel').addEventListener('click',closeModelForm)
+document.getElementById('e-btn-cancel').addEventListener('click',closeModelForm)
+
 function closeModelForm(){
     document.getElementById('overlay').classList.add('hidden')
+    document.getElementById('form-add').reset();
+    document.getElementById('form-edit').reset();
 }
-document.getElementById('btn-add').addEventListener('click',openModelForm)
+document.getElementById('btn-add').addEventListener('click',()=> {openModelForm()})
 // document.getElementById('btn-submit-add').addEventListener('',addHandler)
  window.addHandler = function(ev){
     // alert("ok")
 ev.preventDefault();
     const newPlayer = getData();
     // console.log(newPlayer);
+    list = JSON.parse(localStorage.getItem('FUT-players')) || []; 
     list.push(newPlayer);
     localStorage.setItem('FUT-players',JSON.stringify(list))
+    // window.location.reload();
+    dispalyPlayers(list,'playerList2')
     // console.log("343434",ev);
-    document.getElementById('overlay').classList.add('hidden')
+    closeModelForm();
 }
 
-function getData() {
-    // Get form elements
-    const name = document.getElementById('name').value;
-    const photo = document.getElementById('photo').value;
-    const position = document.getElementById('position').value;
-    const nationality = document.getElementById('nationality').value;
-    const club = document.getElementById('club').value;
-    const logo = document.getElementById('logo').value;
-    const rating = parseInt(document.getElementById('rating').value, 10);
-    const pace = parseInt(document.getElementById('pace').value, 10);
-    const shooting = parseInt(document.getElementById('shooting').value, 10);
-    const passing = parseInt(document.getElementById('passing').value, 10);
-    const dribbling = parseInt(document.getElementById('dribbling').value, 10);
-    const defending = parseInt(document.getElementById('defending').value, 10);
-    const physical = parseInt(document.getElementById('physical').value, 10); 
-
-    const flag = `https://cdn.sofifa.net/flags/nl.png`;
-   
-    const id = 'p-'+Date.now();
-   
-    return {
-      id,
-      name,
-      photo,
-      position,
-      nationality,
-      flag,
-      club,
-      logo,
-      rating,
-      pace,
-      shooting,
-      passing,
-      dribbling,
-      defending,
-      physical
-    };
+window.editHandler = function(ev){
+    ev.preventDefault();
+    const newdata =getDataEdit()
     
-  }
-  
+}
 
 //add
 
 
 
 // remove
+window.deletePlayer = (event)=>{
+    event.stopPropagation();
+    event.preventDefault();
+    const selectedPlayerID = event.target.closest('button').dataset.playerid;
+    // const index = list.findIndex(plyr=>plyr.id==selectedPlayerID);
+    // console.log("deletePleyer",index);
+    removeFromList(selectedPlayerID);
+   //    console.log(213,selectedPlayerID);
+}
 // console.log(d);
 
 // update
+window.updatePlayer = (event)=>{
+    event.stopPropagation();
+    event.preventDefault();
+    const selectedPlayerID = event.target.closest('button').dataset.playerid;
+    openModelForm("edit");
+    displayDetails(selectedPlayerID);
+
+}
+// updatePlayerList(selectedPlayerID);
