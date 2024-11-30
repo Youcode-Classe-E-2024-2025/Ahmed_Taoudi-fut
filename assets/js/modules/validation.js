3d671ee54d009a8b869923ba6973f33a450e1302
Tawdi <ahmedtawdi00@gmail.com>
2024-11-30 21:59:28 +0100

    modifier legic de validation de position

--- assets/js/modules/validation.js
+++ assets/js/modules/validation.js
@@ -2,8 +2,15 @@
 
 
 export function ValidatePos(players,Pos){
+    let savedFormation = JSON.parse(localStorage.getItem('FUT-formation')) || defaultFormation ;
+    Object.values(savedFormation.players).includes('24')
     players.forEach(plyr=> {
-        if(plyr.dataset.position==Pos){
+        // console.log("list : ",Object.values(savedFormation.players),"id : ",plyr.dataset.id);
+        if(Object.values(savedFormation.players).includes(plyr.dataset.id)){
+              plyr.classList.add('hidden');
+            }
+        else if(plyr.dataset.position==Pos){
+
             plyr.classList.remove('hidden');
         }else{
             plyr.classList.add('hidden');
